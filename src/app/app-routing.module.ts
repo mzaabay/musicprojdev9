b2b8dfe9476e1f447d88441a9da4bfb8be12652c
mzaabay <mwantoymou.zaabay@gmaill.com>
2023-01-13 13:39:49 +0100

    url des onglets

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BoutiqueComponent } from './boutique/boutique.component';
+import { ConnexionComponent } from './connexion/connexion.component';
+import { EvenementsComponent } from './evenements/evenements.component';
+import { MessageComponent } from './message/message.component';
+import { PanierComponent } from './panier/panier.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { component: ConnexionComponent, path: 'login' },
+  { component: EvenementsComponent, path: 'evenements' },
+  { component: BoutiqueComponent, path: 'boutique' },
+  { component: PanierComponent, path: 'login/panier' },
+  { component: MessageComponent, path: 'login/messages' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
