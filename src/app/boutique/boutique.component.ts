fab99bfb46dc78fefc956e0f095a3eb8472a0940
hanssen-belen <belen.hanssen.marin@gmail.com>
2023-01-13 16:38:53 +0100

    boutique

--- src/app/boutique/boutique.component.ts
+++ src/app/boutique/boutique.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-boutique',
   templateUrl: './boutique.component.html',
   styleUrls: ['./boutique.component.css']
 })
-export class BoutiqueComponent {
+export class BoutiqueComponent implements OnInit {
+
+  products: any;
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+
+    this.http.get('http://localhost:8289/produits/all');
+  }
 
 }
