a3ebab461dadaac5850d9dffc222c25690132d60
Syo-Nara <souheil.naouel@gmail.com>
2023-01-16 12:05:15 +0100

    register fRegister

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -9,25 +9,13 @@ import { Router } from '@angular/router';
 })
 export class RegisterComponent implements OnInit {
   user: any;
+
   ngOnInit(): void {
     throw new Error('Method not implemented.');
   }
 
   constructor(private http: HttpClient, private route: Router) { }
 
-  connexion(value: any) {
-    this.http.post('http://localhost:8289/login', value).subscribe({
-      next: (data) => {
-        this.user = data;
-        if (this.user != null) {
-          sessionStorage.setItem('userConnected', JSON.stringify(this.user));
-          this.route.navigateByUrl('boutique');
-        } else {
-          this.msg = 'identifiant ou mdp inccorect';
-        }
-      },
-      error: (err) => { console.log(err) }
-    });
-  }
+
 
 }
