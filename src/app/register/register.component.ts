eb47e32422c2ef1e8bcbda9df432067d9adffcb2
Syo-Nara <souheil.naouel@gmail.com>
2023-01-16 12:02:49 +0100

    Added register front

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -8,12 +8,26 @@ import { Router } from '@angular/router';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  user: any;
   ngOnInit(): void {
     throw new Error('Method not implemented.');
   }
 
   constructor(private http: HttpClient, private route: Router) { }
 
-
+  connexion(value: any) {
+    this.http.post('http://localhost:8289/login', value).subscribe({
+      next: (data) => {
+        this.user = data;
+        if (this.user != null) {
+          sessionStorage.setItem('userConnected', JSON.stringify(this.user));
+          this.route.navigateByUrl('boutique');
+        } else {
+          this.msg = 'identifiant ou mdp inccorect';
+        }
+      },
+      error: (err) => { console.log(err) }
+    });
+  }
 
 }
