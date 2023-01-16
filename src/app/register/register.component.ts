a9d175a750a9a4e0251c7a5d839124719820c472
Syo-Nara <souheil.naouel@gmail.com>
2023-01-16 12:40:53 +0100

    error fixed

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -17,7 +17,7 @@ export class RegisterComponent implements OnInit {
   constructor(private http: HttpClient, private route: Router) { }
 
   register(value: any) {
-    this.http.post('')
+
   }
 
 }
