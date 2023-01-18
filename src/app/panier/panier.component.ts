ae23d4edade56ba82cbf27948b16a17c55933a1d
Teckto78 <doryan.smague@gmail.com>
2023-01-18 11:42:48 +0100

    modification quantite directement dans panier

--- src/app/panier/panier.component.ts
+++ src/app/panier/panier.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { UtilityService } from '../utility.service';
 
 @Component({
   selector: 'app-panier',
@@ -8,31 +9,24 @@ import { Router } from '@angular/router';
   styleUrls: ['./panier.component.css']
 })
 export class PanierComponent implements OnInit {
+  @ViewChild('quantity') public quantity: any
   panier: any;
   evenement: any
   produit: any
   msg: any
-  constructor(private http: HttpClient, private route: Router) { }
+  id: any;
+  constructor(private http: HttpClient, private route: Router, public service: UtilityService) { }
 
   ngOnInit(): void {
     this.http.get('http://localhost:8289/panier').subscribe({
       next: (data) => { this.panier = data },
       error: (err) => { console.log(err) }
-
-
-
     });
 
     this.http.get('http://localhost:8289/billeterie').subscribe({
       next: (data) => { this.evenement = data },
       error: (err) => { console.log(err) }
-
-
-
     });
-
-
-
   }
 
 
@@ -88,6 +82,40 @@ export class PanierComponent implements OnInit {
     });
 
 
+
+  }
+
+
+
+  modifQuantite(id_produit: any, quantite: any) {
+    if (this.service.isConnected()) {                                                     //Vérifie si on est bien connecté
+      this.http.get('http://localhost:8289/panier/produit/' + id_produit).subscribe({
+        next: (data) => {
+          this.id = Object.values(data).map(item => item.id).pop()
+          this.http.put('http://localhost:8289/panier/' + this.id, {
+            "id": this.id,
+            "quantite": quantite,
+            "produits": {
+              "id": id_produit
+            }
+          }).subscribe({
+            next: (data) => {
+              this.produit = data;
+              this.ngOnInit();
+            },
+            error: (err) => { console.log(err) }
+          });
+        },
+        error: (err) => { console.log(err) }
+      });
+    }
+    else {
+      this.msg = 'veuillez vous connecter';
+    }
+
+
+
+    localStorage.removeItem('id')
 
   }
 
